c5b3bf4a6cac74ddb89bdb11171711cb5f07c235
สมชาย ทุมอาริยะ <63481258+somchaithumariya@users.noreply.github.com>
2022-01-26 23:35:45 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-    function downloadImage(){
-    var input = document.getElementById("data").value;
-    var name = "chaiqr"
-    var url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+input
-      fetch(url)
-        .then(resp => resp.blob())
-        .then(blob => {
-            const url = window.URL.createObjectURL(blob);
-            const a = document.createElement('a');
-            a.style.display = 'none';
-            a.href = url;
-            // the filename you want
-            a.download = name;
-            document.body.appendChild(a);
-            a.click();
-            window.URL.revokeObjectURL(url);
-        })
-        .catch(() => alert('An error sorry'));
+const input = document.getElementById("data");
+const button = document.getElementById("btn");
+const qrImg = document.getElementById("img");
+
+function downloadImage(){
+  var input = document.getElementById("data").value;
+  var name = "chaiqr"
+  var url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+input
+    fetch(url)
+      .then(resp => resp.blob())
+      .then(blob => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.style.display = 'none';
+          a.href = url;
+          // the filename you want
+          a.download = name;
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+      })
+      .catch(() => alert('An error sorry'));
 }
+
+button.addEventListener("click", ()=>{
+    let data = input.value;
+    if(data.length>0){
+        let qrsource="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+data;
+
+        qrImg.src=qrsource;
+    }
+})
