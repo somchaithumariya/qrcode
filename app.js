8063e87ce06371a4a0f1ea8c0b57f0bee5dfe9f8
สมชาย ทุมอาริยะ <63481258+somchaithumariya@users.noreply.github.com>
2022-01-26 23:46:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,23 +3,23 @@ const button = document.getElementById("btn");
 const qrImg = document.getElementById("img");
 
 function downloadImage(){
-  var input = document.getElementById("data").value;
-  var name = "chaiqr"
-  var url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+input
-    fetch(url)
-      .then(resp => resp.blob())
-      .then(blob => {
-          const url = window.URL.createObjectURL(blob);
-          const a = document.createElement('a');
-          a.style.display = 'none';
-          a.href = url;
-          // the filename you want
-          a.download = name;
-          document.body.appendChild(a);
-          a.click();
-          window.URL.revokeObjectURL(url);
-      })
-      .catch(() => alert('An error sorry'));
+    var input = document.getElementById("data").value;
+    var name = "ChaiQR "
+    var url = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+input
+      fetch(url)
+        .then(resp => resp.blob())
+        .then(blob => {
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            // the filename you want
+            a.download = name;
+            document.body.appendChild(a);
+            a.click();
+            window.URL.revokeObjectURL(url);
+        })
+        .catch(() => alert('กรุณากรอกข้อมูลแล้วสร้าง QR CODE ก่อน'));
 }
 
 button.addEventListener("click", ()=>{
